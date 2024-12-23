0387172bd67d3cb4b49a0eb9bb1a7d2ffd69fce3
anthony <133702426+antb9@users.noreply.github.com>
2024-12-22 22:50:22 -0500

    Update Loading_data.R

--- Loading_data.R
+++ Loading_data.R
@@ -1,4 +1,4 @@
-filepath<-"/Users/anthonybonilla/Desktop/Anthony/Datasets/household_power_consumption.txt"
+filepath<-".data/household_power_consumption.txt"
 data<- read.table(filepath, header= TRUE, sep = ";",na="?") #reading data
 data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
 df <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
